59aa35edbe43eb7af20a2ac64f01b6581e211dfa
Jowita Jońca <Jowita.Jonca@microsoft.wsei.edu.pl>
2022-06-26 12:40:13 +0200

    Dodanie projektow bazy danych oraz GUI. Dodanie nugetow dla EntityFramework. Stworzenie klasy AppDbContext oraz AppDbContextFactory

---
+++ Przedszkole.Database/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Przedszkole.Database
+{
+    public class AppDbContext:DbContext
+    {
+        // Konstruktor bez parametrów
+        public AppDbContext()
+        {
+        }
+
+
+         // Konstruktor z parametrami z dziedziczacej klasy 
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+
+    }
+}
---
+++ Przedszkole.Database/AppDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Przedszkole.Database
+{
+    public class AppDbContextFactory : IDesignTimeContextFactory<AppDbContext>
+    {
+
+        // Zdefiniowanie parametrow i utworzenie AppDbContext
+        public AppDbContext CreateDbContext(string [] args = null)
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>();
+            options.UseSqlServer("Server=.; Database=Przedszkole; Trusted_Connection=True;");
+            return new AppDbContext(options.Options);
+
+        }
+    }
+}
