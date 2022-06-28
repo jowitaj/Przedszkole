8bb63156743fe245559218606d89667a91d8aa3a
Jowita Jońca <jowita.jonca@microsoft.wsei.edu.pl>
2022-06-29 01:23:22 +0200

    Migracja i poprawka

--- Przedszkole.Database/AppDbContextFactory.cs
+++ Przedszkole.Database/AppDbContextFactory.cs
@@ -4,15 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace Przedszkole.Database
 {
-    public class AppDbContextFactory : IDesignTimeContextFactory<AppDbContext>
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
     {
 
         public AppDbContext CreateDbContext(string[] args = null)
         {
-            var options = new DbContextOptionsBuilder();
+            var options = new DbContextOptionsBuilder<AppDbContext>();
             options.UseSqlServer("Server=.; Database=Przedszkole; Trusted_Connection=True");
             return new AppDbContext(options.Options);
 
---
+++ Przedszkole.Database/Migrations/20220628232237_Poczatek.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Przedszkole.Database.Migrations
+{
+    public partial class Poczatek : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Rodzice",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImieMatki = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NazwiskoMatki = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImieOjca = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NazwiskoOjca = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rodzice", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Wychowawcy",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Imie = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nazwisko = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Wychowawcy", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Dzieci",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Imie = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nazwisko = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RodziceId = table.Column<int>(type: "int", nullable: false),
+                    WychowawcaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dzieci", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Dzieci_Rodzice_RodziceId",
+                        column: x => x.RodziceId,
+                        principalTable: "Rodzice",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Dzieci_Wychowawcy_WychowawcaId",
+                        column: x => x.WychowawcaId,
+                        principalTable: "Wychowawcy",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Obecnosci",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Data = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DzieckoId = table.Column<int>(type: "int", nullable: false),
+                    Obecny = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Obecnosci", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Obecnosci_Dzieci_DzieckoId",
+                        column: x => x.DzieckoId,
+                        principalTable: "Dzieci",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dzieci_RodziceId",
+                table: "Dzieci",
+                column: "RodziceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dzieci_WychowawcaId",
+                table: "Dzieci",
+                column: "WychowawcaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Obecnosci_DzieckoId",
+                table: "Obecnosci",
+                column: "DzieckoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Obecnosci");
+
+            migrationBuilder.DropTable(
+                name: "Dzieci");
+
+            migrationBuilder.DropTable(
+                name: "Rodzice");
+
+            migrationBuilder.DropTable(
+                name: "Wychowawcy");
+        }
+    }
+}
