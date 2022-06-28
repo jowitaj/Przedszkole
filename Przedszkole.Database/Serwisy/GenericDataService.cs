25782681de83604aa26ccfc053e7b545c8004deb
Jowita <jowita@Windows-PC.localdomain>
2022-06-29 01:13:41 +0200

    Uzupelnienie serwisow

--- Przedszkole.Database/Serwisy/GenericDataService.cs
+++ Przedszkole.Database/Serwisy/GenericDataService.cs
@@ -1,41 +1,46 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Przedszkole.Database.Services;
 
 public class GenericDataService<T> : IDataService<T> where T : class
 {
     private readonly AppDbContextFactory _contextFactory;
+    private readonly NonQueryDataService<T> _nonQueryData;
 
 
     public GenericDataService(AppDbContextFactory contextFactory)
     {
         _contextFactory = contextFactory;
+        _nonQueryData = new NonQueryDataService<T>(contextFactory);
     }
     
     
-    public Task<IEnumerable<T>> GetAll()
+    public async Task<IEnumerable<T>> GetAll()
     {
-        throw new System.NotImplementedException();
+        using AppDbContext context = _contextFactory.CreateDbContext();
+        return await context.Set<T>().ToListAsync();
     }
 
-    public Task<T> Get(int id)
+    public async Task<T> Get(int id)
     {
-        throw new System.NotImplementedException();
+        using AppDbContext context = _contextFactory.CreateDbContext();
+        return await context.Set<T>().FindAsync(id);
     }
 
-    public Task<T> Create(T entity)
+    public async Task<T> Create(T entity)
     {
-        throw new System.NotImplementedException();
+        return await _nonQueryData.Create(entity);
     }
 
-    public Task<bool> Delete(T entity)
+    public async Task<bool> Delete(T entity)
     {
-        throw new System.NotImplementedException();
+        return await _nonQueryData.Delete(entity);
     }
 
-    public Task<T> Update(T entity)
+    public async Task<T> Update(T entity)
     {
-        throw new System.NotImplementedException();
+        return await _nonQueryData.Update(entity);
     }
 }
--- Przedszkole.Database/Serwisy/NonQueryDataService.cs
+++ Przedszkole.Database/Serwisy/NonQueryDataService.cs
@@ -1,9 +1,43 @@
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace Przedszkole.Database.Services;
 
 public class NonQueryDataService<T> where T : class
 {
+    private readonly NonQueryDataService<T> _nonQueryData;
+    private readonly AppDbContextFactory _contextFactory;
+
+    public NonQueryDataService(AppDbContextFactory contextFactory)
+    {
+        _contextFactory = contextFactory;
+    }
     
+    public async Task<T> Create(T entity)
+    {
+ 
+        using AppDbContext context = _contextFactory.CreateDbContext();
+        EntityEntry<T> createdResult = await context.Set<T>().AddAsync(entity);
+        await context.SaveChangesAsync();
+        return createdResult.Entity;
+    }
+ 
+    public async Task<bool> Delete(T entity)
+    {
+ 
+        using AppDbContext context = _contextFactory.CreateDbContext();
+        context.Set<T>().Remove(entity);
+        await context.SaveChangesAsync();
+        return true;
+    }
+ 
+ 
+    public async Task<T> Update(T entity)
+    {
+        using AppDbContext context = _contextFactory.CreateDbContext();
+        context.Set<T>().Update(entity);
+        await context.SaveChangesAsync();
+        return entity;
+    }
 
 }
