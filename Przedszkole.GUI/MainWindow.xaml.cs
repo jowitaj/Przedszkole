1b4035ae97de13fca332f9cb438399625e5e3903
Jowita Jońca <jowita.jonca@microsoft.wsei.edu.pl>
2022-06-29 03:04:08 +0200

    GUI start

--- Przedszkole.GUI/MainWindow.xaml.cs
+++ Przedszkole.GUI/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Przedszkole.Database.Services.SerwisyModele;
 
 namespace Przedszkole.GUI
 {
@@ -20,9 +21,38 @@ namespace Przedszkole.GUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly DzieckoSevice _dzieckoSevice;
+        private readonly WychowawcaService _wychowawcaService;
+        private readonly RodziceService _rodziceService;
+        private readonly ObecnoscService _obecnoscService;
         public MainWindow()
         {
             InitializeComponent();
+            _dzieckoSevice = new DzieckoSevice();
+            _obecnoscService = new ObecnoscService();
+            _rodziceService = new RodziceService();
+            _wychowawcaService = new WychowawcaService();
+        }
+        
+
+        private void Rodzice_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Rodzice();
+        }
+
+        private void Dzieci_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Dzieci();
+        }
+
+        private void Obecnosci_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Obecnosci();
+        }
+
+        private void Wychowawcy_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Wychowawcy();
         }
     }
 }
