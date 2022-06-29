13797f1752198101840523b8027ded8188624c44
Jowita Jońca <jowita.jonca@microsoft.wsei.edu.pl>
2022-06-29 02:22:12 +0200

    Dodanie dziecko serwis

---
+++ Przedszkole.Database/Serwisy/SerwisyModele/DzieckoSevice.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Przedszkole.Database.Models;
+
+namespace Przedszkole.Database.Services.SerwisyModele;
+
+public class DzieckoSevice
+{
+    private readonly IDataService<Dziecko> _service;
+    public DzieckoSevice()
+    {
+        _service = new GenericDataService<Dziecko>(new AppDbContextFactory());
+    }
+
+    public async Task<Dziecko> Create(string imie, string nazwisko,int rodziceId, int wychowawcaId)
+    {
+        try
+        {
+            Dziecko dziecko = new Dziecko()
+            {
+                Imie = imie,
+                Nazwisko = nazwisko,
+                RodziceId = rodziceId,
+                WychowawcaId = wychowawcaId
+            };
+            return await _service.Create(dziecko);
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+    public async Task<Dziecko> Update(int id,string imie, string nazwisko,int rodziceId, int wychowawcaId)
+    {
+        try
+        {
+            Dziecko dzieckoDoZmiany = await GetById(id);
+            dzieckoDoZmiany.Imie = imie;
+            dzieckoDoZmiany.Nazwisko = nazwisko;
+            dzieckoDoZmiany.RodziceId= rodziceId;
+            dzieckoDoZmiany.WychowawcaId = wychowawcaId;
+             return await _service.Update(dzieckoDoZmiany);
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+    
+    public Task<Dziecko> GetById(int ID)
+    {
+        try
+        {
+            return _service.Get(ID);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+    
+    public async Task<bool> Delete(int id)
+    {
+        try
+        {
+            Dziecko dzieckoDoUsuniecia = await GetById(id);
+
+            return await _service.Delete(dzieckoDoUsuniecia);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+    
+    public async Task<List<Dziecko>> GetAll()
+    {
+        try
+        {
+            return (List<Dziecko>) await _service.GetAll();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+    
+    
+    
+}
